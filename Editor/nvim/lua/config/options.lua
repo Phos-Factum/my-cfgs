c811aa0c90618b55b7deffe30a5c412e75b1ff4c
Max <phosphorus.work@gmail.com>
2024-09-21 16:03:10 +0300

    Toggle-touchpad script + reconfig of LVim

--- Editor/nvim/lua/config/options.lua
+++ Editor/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.o
+
+o.shiftwidth = 4
+o.tabstop = 4
+o.smarttab = true
+o.expandtab = true
+o.smartindent = true
